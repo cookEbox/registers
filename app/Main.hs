f4af82c593c31db5a4d4082a60cf5348d2ab6189
Nick Cooke <nj.cooke@outlook.com>
2024-07-15 16:59:00 +0100

    lesson 2

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,21 @@
 module Main where
 
 import qualified Data.ByteString    as B
-import           Data.Word          (Word8)
+import           Data.Word          (Word8, Word16)
 import           System.Environment (getArgs)
 
-data Binary = I | O deriving (Show, Eq)
+data Bin = I | O deriving (Show, Eq)
 
-data Byte = B  Binary Binary Binary Binary Binary Binary Binary Binary
-          | B2 Binary Binary Binary Binary Binary Binary Binary Binary
-               Binary Binary Binary Binary Binary Binary Binary Binary
-          | B3 Binary Binary Binary Binary Binary Binary Binary Binary
-               Binary Binary Binary Binary Binary Binary Binary Binary
-               Binary Binary Binary Binary Binary Binary Binary Binary
-          | B4 Binary Binary Binary Binary Binary Binary Binary Binary
-               Binary Binary Binary Binary Binary Binary Binary Binary
-               Binary Binary Binary Binary Binary Binary Binary Binary
-               Binary Binary Binary Binary Binary Binary Binary Binary
+data Byte = B  Bin Bin Bin Bin Bin Bin Bin Bin
+          | B2 Bin Bin Bin Bin Bin Bin Bin Bin
+               Bin Bin Bin Bin Bin Bin Bin Bin
+          | B3 Bin Bin Bin Bin Bin Bin Bin Bin
+               Bin Bin Bin Bin Bin Bin Bin Bin
+               Bin Bin Bin Bin Bin Bin Bin Bin
+          | B4 Bin Bin Bin Bin Bin Bin Bin Bin
+               Bin Bin Bin Bin Bin Bin Bin Bin
+               Bin Bin Bin Bin Bin Bin Bin Bin
+               Bin Bin Bin Bin Bin Bin Bin Bin
           deriving (Show, Eq)
 
 instance Semigroup Byte where
@@ -34,17 +34,21 @@ instance Semigroup Byte where
 byteStringToWords :: B.ByteString -> [Word8]
 byteStringToWords = B.unpack
 
-wordToBinaryList :: [Binary] -> Word8 -> [Binary]
-wordToBinaryList acc w  | w > 0 = wordToBinaryList (acc <> [bin]) $ div w 2
+wordToBinList :: [Bin] -> Word8 -> [Bin]
+wordToBinList acc w  | w > 0 = wordToBinList (acc <> [bin]) $ div w 2
                         | w == 0 = reverse acc
                         | otherwise = acc
                         where
                           bin = if rem w 2 == 1 then I else O
 
-boolsToByte :: [Binary] -> Either String Byte
-boolsToByte [b1, b2, b3, b4, b5, b6, b7, b8]
-              = Right $ B b1 b2 b3 b4 b5 b6 b7 b8
-boolsToByte _ = Left "This is not an 8 bit binary number"
+boolsToByte :: [Bin] -> Either String Byte
+boolsToByte byte | length byte < 8 = eightBinToByte $ replicate (8 - length byte) O <> byte
+                 | otherwise       = eightBinToByte byte
+
+eightBinToByte :: [Bin] -> Either String Byte
+eightBinToByte [b1, b2, b3, b4, b5, b6, b7, b8]
+                 = Right $ B b1 b2 b3 b4 b5 b6 b7 b8
+eightBinToByte _ = Left "This is not an 8 bit binary number"
 
 -- replace with foldr as in main
 bytesToByte :: [Either String Byte] -> Either String Byte
@@ -62,13 +66,17 @@ bytesToBytes234 [_]                                      = [Left "This has an in
 bytesToBytes234 [_, Right (B O O _ _ _ I I O)]           = [Left "For a 00* you need at least one extra byte"]
 bytesToBytes234 [_, Right (B I O _ _ _ _ _ _),_]         = [Left "For a 10 you need at least two extra bytes"]
 bytesToBytes234 [_, Right (B O I _ _ _ _ _ _)]           = [Left "For a 01 you need at least one extra byte"]
+bytesToBytes234 [Right (B I O I I I _ _ _),_]            = [Left "For a 10111 you need at least two extra bytes"]
 bytesToBytes234 (_: Left msg : _ )                       = [Left msg]
+bytesToBytes234 (a@(Right (B I O I I O _ _ _)):b:cs)     = bytesToByte [a, b]       : bytesToBytes234 cs
+bytesToBytes234 (a@(Right (B I O I I I _ _ _)):b:c:ds)   = bytesToByte [a, c, b]    : bytesToBytes234 ds
 bytesToBytes234 (a:b@(Right (B I I _ _ _ _ _ _)):cs)     = bytesToByte [a, b]       : bytesToBytes234 cs
-bytesToBytes234 (a@(Right (B _ _ _ _ I _ _ _)):
-                 b@(Right (B O O _ _ _ I I O)):c:ds)     = bytesToByte [a, b, c]    : bytesToBytes234 ds
+-- need to look at the below definition as I'm not sure what the I in a is checking
+bytesToBytes234 (a@(Right (B _ _ _ _ _ _ _ I)):
+                 b@(Right (B O O _ _ _ I I O)):c:d:es)   = bytesToByte [a, b, c, d] : bytesToBytes234 es
 bytesToBytes234 (a:b@(Right (B O O _ _ _ _ _ _)):cs)     = bytesToByte [a, b]       : bytesToBytes234 cs
 bytesToBytes234 (a:b@(Right (B O I _ _ _ _ _ _)):c:ds)   = bytesToByte [a, b, c]    : bytesToBytes234 ds
-bytesToBytes234 (a:b@(Right (B I O _ _ _ _ _ _)):c:d:es) = bytesToByte [a, b, c, d] : bytesToBytes234 es
+bytesToBytes234 (a:b@(Right (B I O _ _ _ _ _ _)):c:d:es) = bytesToByte [a, b, d, c] : bytesToBytes234 es
 bytesToBytes234 x                                        = x
 
 bytesToBytes2 :: [Either String Byte] -> [Either String Byte] 
@@ -96,56 +104,191 @@ bytes2ToOperation bytes@(B2 I O O O I O _ _ _ _ _ _ _ _ _ _)
 bytes2ToOperation _ = Left "Unkown operator"
 
 -- generalise to any number of bytes
--- need a helper function to turn Byte to word8
--- bytesToRegMem :: (String, Byte) -> Either String (String, Byte)
--- bytesToRegMem (str, bytes@(B3 )) 
+-- need a helper function to turn Byte to word8 or word16
+
+toWord8 :: Bin -> Bin -> Bin -> Bin -> Bin -> Bin -> Bin -> Bin 
+        -> String
+toWord8 a b c d e f g h 
+  = show $ sum $ zipWith (\x y -> if z x then y else 0) 
+    [a,b,c,d,e,f,g,h] (reverse $ ((2^)::Word8 -> Word8) <$> [0..7]) 
+
+plusToWord8 :: Bin -> Bin -> Bin -> Bin -> Bin -> Bin -> Bin -> Bin 
+            -> String
+plusToWord8 a b c d e f g h 
+  | worded == "0" = ""
+  | otherwise     = " + " <> worded
+    where worded  = toWord8 a b c d e f g h
+
+toWord16 :: Bin -> Bin -> Bin -> Bin -> Bin -> Bin -> Bin -> Bin 
+         -> Bin -> Bin -> Bin -> Bin -> Bin -> Bin -> Bin -> Bin 
+         -> String
+toWord16 a b c d e f g h i j k l m n o p 
+  = show $ sum $ zipWith (\x y -> if z x then y else 0) 
+    [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p] 
+    (reverse $ ((2^)::Word16 -> Word16) <$> [0..15])
+
+
+plusToWord16 :: Bin -> Bin -> Bin -> Bin -> Bin -> Bin -> Bin -> Bin 
+             -> Bin -> Bin -> Bin -> Bin -> Bin -> Bin -> Bin -> Bin 
+             -> String
+plusToWord16 a b c d e f g h i j k l m n o p 
+  | worded == "0" = ""
+  | otherwise     = " + " <> worded
+    where worded  = toWord16 a b c d e f g h i j k l m n o p 
+
+bytesToRegMem :: (String, Byte) -> Either String (String, Byte)
+bytesToRegMem (str, bytes@(B2 I O I I O a1 a2 a3 b1 b2 b3 b4 b5 b6 b7 b8))
+               = Right ( str <> " " <> thinReg a1 a2 a3 
+                             <> ", " <> toWord8 b1 b2 b3 b4 b5 b6 b7 b8
+                             , bytes
+                       )
+bytesToRegMem (str, bytes@(B3 I O I I I a1 a2 a3 b1 b2 b3 b4 b5 b6 b7 b8 c1 c2 c3 c4 c5 c6 c7 c8))
+               = Right ( str <> " " <> wideReg a1 a2 a3 
+                             <> ", " <> toWord16 b1 b2 b3 b4 b5 b6 b7 b8 c1 c2 c3 c4 c5 c6 c7 c8
+                             , bytes
+                       )
+bytesToRegMem (str, bytes@(B4 _ _ _ _ _ _ I _ O O a1 a2 a3 I  O  O
+                                                  c1 c2 c3 c4 c5 c6 c7 c8 
+                                                  d1 d2 d3 d4 d5 d6 d7 d8
+                          ))
+               = Right ( str <> " " <> wideReg a1 a2 a3 <> ", " 
+                             <> "[" 
+                             <> toWord16 c1 c2 c3 c4 c5 c6 c7 c8 d1 d2 d3 d4 d5 d6 d7 d8
+                             <> "]"
+                             , bytes
+                       ) 
+bytesToRegMem (str, bytes@(B4 _ _ _ _ _ _ O _ O O I  O  O  b1 b2 b3
+                                                  c1 c2 c3 c4 c5 c6 c7 c8 
+                                                  d1 d2 d3 d4 d5 d6 d7 d8
+                          ))
+               = Right ( str <> " " <> "[" 
+                             <> toWord16 c1 c2 c3 c4 c5 c6 c7 c8 d1 d2 d3 d4 d5 d6 d7 d8
+                             <> "]"
+                             <> ", " <> wideReg b1 b2 b3 
+                             , bytes
+                       ) 
+bytesToRegMem (str, bytes@(B4 _ _ _ _ _ _ d w I O a1 a2 a3 b1 b2 b3 
+                                                  c1 c2 c3 c4 c5 c6 c7 c8 
+                                                  d1 d2 d3 d4 d5 d6 d7 d8
+                          ))
+  | z d && z w = Right ( str <> " " <> wideReg a1 a2 a3 <> ", " 
+                             <> "[" <> ooMem   b1 b2 b3 
+                             <> plusToWord16 c1 c2 c3 c4 c5 c6 c7 c8 d1 d2 d3 d4 d5 d6 d7 d8
+                             <> "]"
+                             , bytes
+                       ) 
+  | z d        = Right ( str <> " " <> thinReg a1 a2 a3 <> ", " 
+                             <> "[" <> ooMem   b1 b2 b3 
+                             <> plusToWord16 c1 c2 c3 c4 c5 c6 c7 c8 d1 d2 d3 d4 d5 d6 d7 d8
+                             <> "]"
+                             , bytes
+                       )
+  |        z w = Right ( str <> " " <> "[" <> ooMem   b1 b2 b3 
+                             <> plusToWord16 c1 c2 c3 c4 c5 c6 c7 c8 d1 d2 d3 d4 d5 d6 d7 d8
+                             <> "]" <> ", " 
+                             <> wideReg a1 a2 a3
+                             , bytes
+                       )
+  | otherwise  = Right ( str <> " " <> "[" <> ooMem   b1 b2 b3 
+                             <> plusToWord16 c1 c2 c3 c4 c5 c6 c7 c8 d1 d2 d3 d4 d5 d6 d7 d8
+                             <> "]" <> ", " <> thinReg a1 a2 a3
+                             , bytes
+                       )
+bytesToRegMem (str, bytes@(B3 _ _ _ _ _ _ d w O I a1 a2 a3 b1 b2 b3 c1 c2 c3 c4 c5 c6 c7 c8))
+  | z d && z w = Right ( str <> " " <> wideReg a1 a2 a3 <> ", " 
+                             <> "[" <> ooMem   b1 b2 b3 
+                             <> plusToWord8 c1 c2 c3 c4 c5 c6 c7 c8 
+                             <> "]"
+                             , bytes
+                       ) 
+  | z d        = Right ( str <> " " <> thinReg a1 a2 a3 <> ", " 
+                             <> "[" <> ooMem   b1 b2 b3 
+                             <> plusToWord8 c1 c2 c3 c4 c5 c6 c7 c8 
+                             <> "]"
+                             , bytes
+                       )
+  |        z w = Right ( str <> " " <> "[" <> ooMem   b1 b2 b3 
+                             <> plusToWord8 c1 c2 c3 c4 c5 c6 c7 c8 
+                             <> "]" <> ", " 
+                             <> wideReg a1 a2 a3
+                             , bytes
+                       )
+  | otherwise  = Right ( str <> " " <> "[" <> ooMem   b1 b2 b3 
+                             <> plusToWord8 c1 c2 c3 c4 c5 c6 c7 c8 
+                             <> "]" <> ", " <> thinReg a1 a2 a3
+                             , bytes
+                       )
+bytesToRegMem (str, bytes@(B2 _ _ _ _ _ _ d w O O a1 a2 a3 b1 b2 b3))
+  | z d && z w = Right (str <> " " <> wideReg a1 a2 a3 <> ", " <> "[" <> ooMem   b1 b2 b3 <> "]", bytes)
+  | z d        = Right (str <> " " <> thinReg a1 a2 a3 <> ", " <> "[" <> ooMem   b1 b2 b3 <> "]", bytes)
+  |        z w = Right (str <> " " <> "[" <> ooMem   b1 b2 b3 <> "]" <> ", " <> wideReg a1 a2 a3, bytes)
+  | otherwise  = Right (str <> " " <> "[" <> ooMem   b1 b2 b3 <> "]" <> ", " <> thinReg a1 a2 a3, bytes)
+bytesToRegMem (str, bytes@(B2 _ _ _ _ _ _ d w I I a1 a2 a3 b1 b2 b3))
+  | z d && z w = Right (str <> " " <> wideReg a1 a2 a3 <> ", " <> wideReg b1 b2 b3, bytes)
+  | z d        = Right (str <> " " <> thinReg a1 a2 a3 <> ", " <> thinReg b1 b2 b3, bytes)
+  |        z w = Right (str <> " " <> wideReg b1 b2 b3 <> ", " <> wideReg a1 a2 a3, bytes)
+  | otherwise  = Right (str <> " " <> thinReg b1 b2 b3 <> ", " <> thinReg a1 a2 a3, bytes)
+bytesToRegMem (_, bytes) = Left $ "This is not a recognized pattern of bytes\n" <> show bytes
+
+z :: Bin -> Bool
+z d = d == I
+
+ooMem, wideReg, thinReg :: Bin -> Bin -> Bin -> String
+ooMem   O O O = "bx + si"
+ooMem   O O I = "bx + di"
+ooMem   O I O = "bp + si"
+ooMem   O I I = "bp + di"
+ooMem   I O O = "si"
+ooMem   I O I = "di"
+ooMem   I I O = "bp"
+ooMem   I I I = "bx"
+wideReg O O O = "ax"
+wideReg O O I = "cx"
+wideReg O I O = "dx"
+wideReg O I I = "bx"
+wideReg I O O = "sp"
+wideReg I O I = "bp"
+wideReg I I O = "si"
+wideReg I I I = "di"
+thinReg O O O = "al"
+thinReg O O I = "cl"
+thinReg O I O = "dl"
+thinReg O I I = "bl"
+thinReg I O O = "ah"
+thinReg I O I = "ch"
+thinReg I I O = "dh"
+thinReg I I I = "bh"
 
 bytes2ToRegisters :: (String, Byte) -> Either String (String, Byte)
 bytes2ToRegisters (str, bytes@(B2 _ _ _ _ _ _ d w I I a1 a2 a3 b1 b2 b3))
-  | db && wb  = Right (str <> " " <> wideReg a1 a2 a3 <> ", " <> wideReg b1 b2 b3, bytes)
-  | db        = Right (str <> " " <> thinReg a1 a2 a3 <> ", " <> thinReg b1 b2 b3, bytes)
-  |       wb  = Right (str <> " " <> wideReg b1 b2 b3 <> ", " <> wideReg a1 a2 a3, bytes)
-  | otherwise = Right (str <> " " <> thinReg b1 b2 b3 <> ", " <> thinReg a1 a2 a3, bytes)
-    where
-      db = d == I
-      wb = w == I
-      wideReg O O O = "ax"
-      wideReg O O I = "cx"
-      wideReg O I O = "dx"
-      wideReg O I I = "bx"
-      wideReg I O O = "sp"
-      wideReg I O I = "bp"
-      wideReg I I O = "si"
-      wideReg I I I = "di"
-      thinReg O O O = "al"
-      thinReg O O I = "cl"
-      thinReg O I O = "dl"
-      thinReg O I I = "bl"
-      thinReg I O O = "ah"
-      thinReg I O I = "ch"
-      thinReg I I O = "dh"
-      thinReg I I I = "bh"
+  | z d && z w = Right (str <> " " <> wideReg a1 a2 a3 <> ", " <> wideReg b1 b2 b3, bytes)
+  | z d        = Right (str <> " " <> thinReg a1 a2 a3 <> ", " <> thinReg b1 b2 b3, bytes)
+  |        z w = Right (str <> " " <> wideReg b1 b2 b3 <> ", " <> wideReg a1 a2 a3, bytes)
+  | otherwise  = Right (str <> " " <> thinReg b1 b2 b3 <> ", " <> thinReg a1 a2 a3, bytes)
 bytes2ToRegisters _ = Left "Not 2 bytes"
+
+--
 
 stringASM :: Either String (String, Byte) -> Either String String
 stringASM (Right (asm, _)) = Right $ asm <> "\n"
 stringASM (Left msg)       = Left msg
 
 registers :: Either String Byte -> Either String String
-registers (Right byte) = stringASM $ bytes2ToRegisters =<< bytes2ToOperation byte
+registers (Right byte) = stringASM $ bytesToRegMem =<< bytes234ToOpCode byte
 registers (Left msg)   = Left msg
 
 main :: IO ()
 main = do
   file     <- fmap head getArgs
   contents <- B.readFile file
+  putStrLn $ concat $ (<>"\n") <$> concatMap show <$> bytesToBytes234 (boolsToByte . wordToBinList [] <$> byteStringToWords contents)
   let op =  foldr (\x acc -> (<>) <$> x <*> acc) (Right "") 
          $  registers
         <$> bytesToBytes234
          (  boolsToByte
-         .  wordToBinaryList  []
+         .  wordToBinList  []
         <$> byteStringToWords contents
          )
   case op of
     Right msg -> putStrLn $ "bits 16 \n\n" <> msg
-    Left  err -> print err
+    Left  err -> putStrLn err

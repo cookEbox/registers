807e469cc64f753c8b04480e2202ea132d03b52a
Nick Cooke <nj.cooke@outlook.com>
2024-07-16 10:50:35 +0100

    lesson 2 - removed unused functions

--- app/Main.hs
+++ app/Main.hs
@@ -50,17 +50,9 @@ eightBinToByte [b1, b2, b3, b4, b5, b6, b7, b8]
                  = Right $ B b1 b2 b3 b4 b5 b6 b7 b8
 eightBinToByte _ = Left "This is not an 8 bit binary number"
 
--- replace with foldr as in main
 bytesToByte :: [Either String Byte] -> Either String Byte
 bytesToByte = foldr1 (\x acc -> (<>) <$> x <*> acc)
 
-bytesToByte2 :: [Either String Byte] -> Either String Byte
-bytesToByte2 [Right fbyte, Right sbyte] = Right $ fbyte <> sbyte
-bytesToByte2 [Left msg, _]              = Left msg
-bytesToByte2 [_, Left msg]              = Left msg
-bytesToByte2 _                          = Left "Too many elements"
-
--- replace with something that checks the second element for if it needs the 3rd and 4th
 bytesToBytes234 :: [Either String Byte] -> [Either String Byte] 
 bytesToBytes234 [_]                                      = [Left "This has an incorrect number of bytes"]
 bytesToBytes234 [_, Right (B O O _ _ _ I I O)]           = [Left "For a 00* you need at least one extra byte"]
@@ -71,7 +63,6 @@ bytesToBytes234 (_: Left msg : _ )                       = [Left msg]
 bytesToBytes234 (a@(Right (B I O I I O _ _ _)):b:cs)     = bytesToByte [a, b]       : bytesToBytes234 cs
 bytesToBytes234 (a@(Right (B I O I I I _ _ _)):b:c:ds)   = bytesToByte [a, c, b]    : bytesToBytes234 ds
 bytesToBytes234 (a:b@(Right (B I I _ _ _ _ _ _)):cs)     = bytesToByte [a, b]       : bytesToBytes234 cs
--- need to look at the below definition as I'm not sure what the I in a is checking
 bytesToBytes234 (a@(Right (B _ _ _ _ _ _ _ I)):
                  b@(Right (B O O _ _ _ I I O)):c:d:es)   = bytesToByte [a, b, c, d] : bytesToBytes234 es
 bytesToBytes234 (a:b@(Right (B O O _ _ _ _ _ _)):cs)     = bytesToByte [a, b]       : bytesToBytes234 cs
@@ -79,12 +70,6 @@ bytesToBytes234 (a:b@(Right (B O I _ _ _ _ _ _)):c:ds)   = bytesToByte [a, b, c]
 bytesToBytes234 (a:b@(Right (B I O _ _ _ _ _ _)):c:d:es) = bytesToByte [a, b, d, c] : bytesToBytes234 es
 bytesToBytes234 x                                        = x
 
-bytesToBytes2 :: [Either String Byte] -> [Either String Byte] 
-bytesToBytes2 bytes@(x:y:zs) | even $ length bytes = bytesToByte2 [x, y] : bytesToBytes2 zs
-                             | otherwise           = [Left "This is not a list of 16 bits"]
-bytesToBytes2 x              = x
-
--- replace with opcode that checks first 6 elements of B2 B3 B4
 bytes234ToOpCode :: Byte -> Either String (String, Byte)
 bytes234ToOpCode bytes@(B2 I O I I _ _ _ _ _ _ _ _ _ _ _ _) 
                    = Right ("mov", bytes)
@@ -97,14 +82,6 @@ bytes234ToOpCode bytes@(B3 I O O O I O _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _)
 bytes234ToOpCode bytes@(B4 I O O O I O _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _) 
                    = Right ("mov", bytes)
 bytes234ToOpCode _ = Left "Unkown operator"
-
-bytes2ToOperation :: Byte -> Either String (String, Byte)
-bytes2ToOperation bytes@(B2 I O O O I O _ _ _ _ _ _ _ _ _ _)
-                    = Right ("mov", bytes)
-bytes2ToOperation _ = Left "Unkown operator"
-
--- generalise to any number of bytes
--- need a helper function to turn Byte to word8 or word16
 
 toWord8 :: Bin -> Bin -> Bin -> Bin -> Bin -> Bin -> Bin -> Bin 
         -> String
@@ -258,16 +235,6 @@ thinReg I O O = "ah"
 thinReg I O I = "ch"
 thinReg I I O = "dh"
 thinReg I I I = "bh"
-
-bytes2ToRegisters :: (String, Byte) -> Either String (String, Byte)
-bytes2ToRegisters (str, bytes@(B2 _ _ _ _ _ _ d w I I a1 a2 a3 b1 b2 b3))
-  | z d && z w = Right (str <> " " <> wideReg a1 a2 a3 <> ", " <> wideReg b1 b2 b3, bytes)
-  | z d        = Right (str <> " " <> thinReg a1 a2 a3 <> ", " <> thinReg b1 b2 b3, bytes)
-  |        z w = Right (str <> " " <> wideReg b1 b2 b3 <> ", " <> wideReg a1 a2 a3, bytes)
-  | otherwise  = Right (str <> " " <> thinReg b1 b2 b3 <> ", " <> thinReg a1 a2 a3, bytes)
-bytes2ToRegisters _ = Left "Not 2 bytes"
-
---
 
 stringASM :: Either String (String, Byte) -> Either String String
 stringASM (Right (asm, _)) = Right $ asm <> "\n"
